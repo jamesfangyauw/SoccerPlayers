1fef9d37110d5006ffe1d5f8110bb9f5212b15e1
James <jamesfangyauw@gmail.com>
2024-07-20 17:24:02 +0700

    add test8

--- app/src/main/java/com/james/soccerplayers/MainActivity.kt
+++ app/src/main/java/com/james/soccerplayers/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     var master6 = 6
     var test7 = 7
     var master7 = 7
+    var test8 = 8
     var master8 = 8
     var master9 = 9
     var master10 = 10
