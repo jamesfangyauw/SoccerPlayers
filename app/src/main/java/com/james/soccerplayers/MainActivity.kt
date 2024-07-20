3953ee722b77d74c2d3414e100ef40b5805d3072
James <jamesfangyauw@gmail.com>
2024-07-20 16:24:29 +0700

    add var on line 17

--- app/src/main/java/com/james/soccerplayers/MainActivity.kt
+++ app/src/main/java/com/james/soccerplayers/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     var test = 1
     var test2  = 2
     var test3 = 3
+    var test4 = 4
 
     private lateinit var activityMainBinding: ActivityMainBinding
 
